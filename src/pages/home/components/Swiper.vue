dd480475c52a3f69a3f4fa9f1527ccbbb14606ba
wjc <15584932588@163.com>
2022-04-22 22:47:16 +0800

    icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,7 +2,7 @@
 <div class="wrapper">
   <swiper :options="swiperOption">
     <!-- slides -->
-      <swiper-slide v-for="item of swiperList" :key="item.id">
+      <swiper-slide v-for="item of list" :key="item.id">
       <img :src="item.imgUrl" alt="" class="swiper-img">
     </swiper-slide>
     <div class="swiper-pagination"  slot="pagination"></div>
@@ -14,19 +14,15 @@
 <script>
 export default{
     name:'HomeSwiper',
+    props:{
+      list:Array
+    },
     data(){
         return {
           swiperOption:{
             pagination:'.swiper-pagination',
             loop:true
-          },
-          swiperList:[{
-            id:'0001',
-            imgUrl:'https://imgs.qunarzz.com/vs_ceph_vcimg/5be2601d1f921f166646b817459a2727.jpeg'
-          },{
-            id:'0002',
-            imgUrl:'https://imgs.qunarzz.com/vs_ceph_vcimg/280673be5655fc91ce24ba605c62fe07.jpeg'
-          }]
+          }
         }
     }
 }
@@ -39,7 +35,7 @@ export default{
       overflow: hidden
       width: 100%
       height: 0
-      padding-bottom: 30%
+      padding-bottom: 31.05%
       background: #eee
     .swiper-img
       width: 100%
